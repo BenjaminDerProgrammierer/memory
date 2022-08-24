659fe696fc4eaf547f7fbb891c0292821e22d9bc
BenjaminDerProgrammierer <benjamin.p.hartmann@gmail.com>
2022-08-24 11:17:29 +0200

    Initial Commit

--- src/main/java/net/coderdojo/linz/memory/PlayerResponse.java
+++ src/main/java/net/coderdojo/linz/memory/PlayerResponse.java
@@ -1,5 +1,9 @@
 package net.coderdojo.linz.memory;
 
+import java.util.List;
+
 public class PlayerResponse {
-    
+    public String name;
+    public boolean currentPlayer;
+    public List<String> foundCards;
 }
