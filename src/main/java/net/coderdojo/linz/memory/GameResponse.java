25c99ef04d98147fe0cc8f3b43eec4549f77978b
BenjaminDerProgrammierer <benjamin.p.hartmann@gmail.com>
2022-08-24 11:27:33 +0200

    Initial Commit

--- src/main/java/net/coderdojo/linz/memory/GameResponse.java
+++ src/main/java/net/coderdojo/linz/memory/GameResponse.java
@@ -1,5 +1,28 @@
 package net.coderdojo.linz.memory;
 
+import java.util.List;
+
 public class GameResponse {
-    
+    public int id;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<String> cards;
+    public List<String> getCards() {
+        return cards;
+    }
+    public void setCards(List<String> cards) {
+        this.cards = cards;
+    }
+    public List<PlayerResponse> players;
+    public List<PlayerResponse> getPlayers() {
+        return players;
+    }
+    public void setPlayers(List<PlayerResponse> players) {
+        this.players = players;
+    }
 }
--- src/main/java/net/coderdojo/linz/memory/PlayerResponse.java
+++ src/main/java/net/coderdojo/linz/memory/PlayerResponse.java
@@ -4,6 +4,26 @@ import java.util.List;
 
 public class PlayerResponse {
     public String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    
     public boolean currentPlayer;
+    public boolean getCurrentPlayer() {
+        return currentPlayer;
+    }
+    public void setCurrentPlayer(boolean currentPlayer) {
+        this.currentPlayer = currentPlayer;
+    }
+
     public List<String> foundCards;
+    public List<String> getFoundCards() {
+        return foundCards;
+    }
+    public void setFoundCards(List<String> foundCards) {
+        this.foundCards = foundCards;
+    }
 }
